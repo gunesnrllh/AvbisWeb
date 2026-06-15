b6dcd251b4e539467ecefa410cd1be38a7141978
nurullahgunes <nurullah.gunes@testinium.com>
2026-06-15 14:56:10 +0300

    key show log added

--- src/test/java/com/testinium/base/BaseTest.java
+++ src/test/java/com/testinium/base/BaseTest.java
@@ -89,7 +89,6 @@ public class BaseTest {
                 capabilities.setCapability(ChromeOptions.CAPABILITY, options);
                 capabilities.setCapability("key", System.getProperty("key"));
                 logger.info("KEY => {}", System.getProperty("key"));
-
                 browserName = System.getenv("browser");
                 driver = new RemoteWebDriver(new URL("http://172.16.82.20:4444/wd/hub"), capabilities);
                 actions = new Actions(driver);
